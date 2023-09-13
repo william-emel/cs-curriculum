f38e7969c334d0050ecb2f7021309439c753db2b
Owen <oschlimgen9040@student.pps.net>
2023-09-13 10:00:55 -0700

    Fixed Animations and Cave Exit

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -4,16 +4,25 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
+    Rigidbody2D rb;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKey(KeyCode.RightArrow))
+        {
+            rb.velocity = new Vector2(3, 0);
+        }
+
+        if(Input.GetKey(KeyCode.LeftArrow))
+        {
+            rb.velocity = new Vector2(-3, 0);
+        }
     }
 }
--- Assets/Scripts/TopDown_AnimatorController.cs
+++ Assets/Scripts/TopDown_AnimatorController.cs
@@ -17,7 +17,7 @@ public class TopDown_AnimatorController : MonoBehaviour
     {
         //start off facing to the right.
         anim.SetBool("isWalking", false);
-        anim.SetInteger("walkDir", 1);
+        anim.SetInteger("walkDir", 2);
         sprite.flipX = true;
 
     }
@@ -33,7 +33,6 @@ public class TopDown_AnimatorController : MonoBehaviour
         //if either horizontal or vertical is not equal to zero but not both
         if (Input.GetAxis("Horizontal") != 0 || Input.GetAxis("Vertical") != 0 && !(Input.GetAxis("Horizontal") != 0 && Input.GetAxis("Vertical") != 0))
         {
-            anim.enabled = true;
             anim.SetBool("isWalking", true);
             if (Input.GetAxis("Horizontal") > 0)
             {
@@ -57,22 +56,12 @@ public class TopDown_AnimatorController : MonoBehaviour
         else
         {
             anim.SetBool("isWalking", false);
-            if (!anim.GetBool("Attack"))
-            {
-                anim.enabled = false;
-            }
         }
 
-        AnimatorStateInfo info = anim.GetCurrentAnimatorStateInfo(0);
-        if (!info.IsName("Shovel_Swing_Horizontal") && !info.IsName("Shovel_Swing_Up") && !info.IsName("Shovel_Swing_Down"))
+        if (Input.GetMouseButton(0))
         {
-            anim.SetBool("Attack", false);
-            if (Input.GetMouseButton(0))
-            {
-                anim.enabled = true;
-                anim.SetBool("Attack", true);
-                anim.SetBool("isWalking", false);
-            }
+            anim.SetTrigger("Attack");
+            anim.SetBool("isWalking", false);
         }
             
     }
