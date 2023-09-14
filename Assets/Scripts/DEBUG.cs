87eb8effa4fbf23e710488130646d55d796756b3
Javyn Andrico <jandrico7658@student.pps.net>
2023-09-14 09:59:49 -0700

    Made coin a prefab
    preparing to join into one branch

---
+++ Assets/Scripts/DEBUG.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement2D : MonoBehaviour
+{
+    Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.RightArrow))
+        {
+            rb.velocity = new Vector2(3, 0);
+        }
+
+        if(Input.GetKey(KeyCode.LeftArrow))
+        {
+            rb.velocity = new Vector2(-3, 0);
+        }
+
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            rb.velocity = new Vector2(0, 3);
+        }
+
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            rb.velocity = new Vector2(0, -3);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        other.gameObject.SetActive(false);
+    }
+}
